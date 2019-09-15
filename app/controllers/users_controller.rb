cdf16216437adfe44fa9744473cfb06e57788856
honda0924 <honda0924@hotmail.co.jp>
2019-09-15 17:12:41 +0900

    fix_delete_path_and_like_order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @name=@user.name
     @blogs=@user.blogs.order("created_at DESC")
-    @likeblogs = @user.liked_blogs
+    @likeblogs = @user.liked_blogs.order("created_at DESC")
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :blogs
-  has_many :likes
+  has_many :blogs, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_blogs, through: :likes, source: :blog
   def already_liked?(blog)
     self.likes.exists?(blog_id: blog.id)
